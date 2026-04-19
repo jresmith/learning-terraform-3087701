97c4c564cc3d6e5a1a0dfd91eb0922316c37c414
jresmith <jresmith@outlook.com>
2026-04-19 14:14:07 -0700

    removed superfluous characters

--- main.tf
+++ main.tf
@@ -38,9 +38,9 @@ resource "aws_security_group" "blog" {
 }
 
 resource "aws_security_group_rule" "blog_http_in" {
-  type.       = "ingress"
+  type        = "ingress"
   from_port   = 80
-  to_port.    = 80
+  to_port     = 80
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 
@@ -49,9 +49,9 @@ resource "aws_security_group_rule" "blog_http_in" {
 
 
 resource "aws_security_group_rule" "blog_https_in" {
-  type.       = "ingress"
+  type        = "ingress"
   from_port   = 443
-  to_port.    = 443
+  to_port     = 443
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 
@@ -59,9 +59,9 @@ resource "aws_security_group_rule" "blog_https_in" {
 }
 
 resource "aws_security_group_rule" "blog_everything_out" {
-  type.       = "egress"
+  type        = "egress"
   from_port   = 0
-  to_port.    = 0
+  to_port     = 0
   protocol    = "-1"
   cidr_blocks = ["0.0.0.0/0"]
 
