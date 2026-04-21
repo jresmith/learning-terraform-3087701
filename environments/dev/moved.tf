36960d5a138ff70081eb9633238bd3f4194632a8
jresmith <jresmith@outlook.com>
2026-04-20 20:12:17 -0700

    fixed typos in moved

--- environments/dev/moved.tf
+++ environments/dev/moved.tf
@@ -4,11 +4,11 @@ moved {
 }
 moved { 
     from = module.blog_sg
-    to = module.dev.module.blog_vpc
+    to = module.dev.module.blog_sg
 }
 moved { 
     from = module.blog_alb
-    to = module.dev.module.blog_vpc
+    to = module.dev.module.blog_alb
 }
 moved { 
     from = aws_lb_target_group.blog
@@ -16,5 +16,5 @@ moved {
 }
 moved { 
     from = module.blog_autoscaling
-    to = module.dev.module.blog_vpc
+    to = module.dev.module.blog_autoscaling
 }
