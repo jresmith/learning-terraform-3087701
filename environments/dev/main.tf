01b4e0f316b52fa863ace64dbb6cef5ac9bece73
jresmith <jresmith@outlook.com>
2026-04-20 20:36:43 -0700

    qa changes

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,3 +1,10 @@
 module "dev" {
     source = "../../modules/blog"
+
+    environment ={
+        name           = "qa"
+        network_prefix = "10.1"
+    }
+    min_size       = 1
+    max_size       = 1
 }
